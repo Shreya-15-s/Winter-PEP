13e272e0b67609777132044f0a3fd3a6523320c7
Shreya-15-s <shreya82195@gmail.com>
2025-12-19 09:27:32 +0530

    Update 02_Repeated_Number_Square_Pattern.cpp

--- C++/General_Programs/Patterns/18-12-2025/02_Repeated_Number_Square_Pattern.cpp
+++ C++/General_Programs/Patterns/18-12-2025/02_Repeated_Number_Square_Pattern.cpp
@@ -8,9 +8,9 @@
 #include<io.stream.h>
 int main()
 {
-  for(int i = 1; i<+5;i++)
+  for(int i = 1; i<=5;i++)
     {
-      for(int j =1; j<+5; j++)
+      for(int j =1; j<=5; j++)
         {
           cout<<i;
         }
